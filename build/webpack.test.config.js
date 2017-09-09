31b21821a642166f2bf7213f36354fa354f65e05
黄书伟 <hsw89520@163.com>
2017-09-09 16:15:36 +0800

    悠哈

--- build/webpack.test.config.js
+++ build/webpack.test.config.js
@@ -1,30 +1,19 @@
 var path = require("path");
 const autoprefixer = require('autoprefixer')
 const px2rem = require('postcss-px2rem')
-var webpack = require("webpack");
+var webpack = require("webpack")
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
 
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
-
-function resolve (dir) {
+function resolve(dir) {
 
     return path.join(__dirname, '..', dir)
 }
 
-var webpackConfig  = {
+var webpackConfig = {
 
     module: {
 
-        rules:[
-
-         /*   {
-                test: /\.(js|vue)$/,
-                use:{
-                    loader: 'eslint-loader',
-                    options: {
-                        formatter: require('eslint-friendly-formatter')
-                    }
-                }
-            },*/
+        rules: [
 
             // babel-loader
             {
@@ -36,22 +25,23 @@ var webpackConfig  = {
             // 为了统计代码覆盖率，对 js 文件加入 istanbul-instrumenter-loader
             {
                 test: /\.(js)$/,
-                loader: 'istanbul-instrumenter-loader',
                 exclude: /node_modules/,
                 include: /src|packages/,
                 enforce: 'post',
-                options: {
-                    esModules: true
-                }
+                use: [{
+                    loader: "istanbul-instrumenter-loader",
+                    options: {
+                        esModules: true
+                    },
+                }]
             },
 
             // vue loader
             {
                 test: /\.vue$/,
-                loaders: [{
+                use: [{
                     loader: 'vue-loader',
                     options: {
-                        postcss: [autoprefixer({browsers: ['> 1%', 'ie >= 9', 'iOS >= 6', 'Android >= 2.1']}), px2rem({remUnit: 75})],
                         // 为了统计代码覆盖率，对 vue 文件加入 istanbul-instrumenter-loader
                         preLoaders: {
                             js: 'istanbul-instrumenter-loader?esModules=true'
@@ -63,11 +53,7 @@ var webpackConfig  = {
             // css loader
             {
                 test: /\.css$/,
-              /*  use: [
-                    'style-loader',
-                    'css-loader'
-                ],*/
-                use:ExtractTextPlugin.extract({
+                use: ExtractTextPlugin.extract({
                     use: 'css-loader',
                     fallback: 'vue-style-loader'
                 })
@@ -76,41 +62,23 @@ var webpackConfig  = {
             // img loader
             {
                 test: /\.(png|gif|jpe?g)(\?\S*)?$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        /*
-                         *  limit=10000 ： 10kb
-                         *  图片小于10kb 采用内联的形式，否则输出图片
-                         * */
-                        limit: 10000,
-                        name: 'images/[name]-[hash:8].[ext]'
-                    }
-                }]
+                use: [{loader: 'url-loader'}]
             },
 
             // font loader
             {
                 test: /\.(eot|woff|woff2|ttf|svg)(\?\S*)?$/,
-                use: [{
-                    loader: 'url-loader',
-                    options: {
-                        limit: 5000,
-                        name: 'font/[name]-[hash:8].[ext]'
-                    }
-                }]
-
+                use: [{loader: 'url-loader'}]
             },
         ]
     },
 
-    resolve:{
+    resolve: {
         extensions: ['.js', '.vue', '.json'],
         alias: {
-            'vue$': 'vue/dist/vue.esm.js', // 'vue/dist/vue'
+            'vue$': 'vue/dist/vue.esm.js',
             '@': resolve('src'),
         }
-
     },
 
     plugins: [
@@ -120,7 +88,6 @@ var webpackConfig  = {
             }
         })
     ]
-
 }
 
 module.exports = webpackConfig
